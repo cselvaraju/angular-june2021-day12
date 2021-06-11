d071575418ea70dc526bb4ff34fff8ce2c3747ce
C Selvaraju <cselvaraju@gmail.com>
2021-06-11 11:58:38 +0530

    Added code

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+
+// Service Imports
+import { DishService } from "./services/dish.service";
+import { PostService } from "./services/post.service";
+
+// Pipe Imports
+import { ReversetextPipe } from "./pipes/reversetext.pipe";
+import { TruncatetextPipe } from "./pipes/truncatetext.pipe";
+
+// Component Imports
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BlogComponent } from "./components/blog/blog.component";
+import { ChildComponent } from "./components/child/child.component";
+import { ParentComponent } from "./components/parent/parent.component";
+import { PipedemoComponent } from "./components/pipedemo/pipedemo.component";
+import { SiblingComponent } from "./components/sibling/sibling.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BlogComponent,
+    ChildComponent,
+    ParentComponent,
+    PipedemoComponent,
+    SiblingComponent,
+    ReversetextPipe,
+    TruncatetextPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    DishService,
+    TruncatetextPipe
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
